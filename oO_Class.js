238741c9283b91a681f3b02e157ad57bb0fab42a
Nabin <nkbhaie2017@gmail.com>
2021-02-26 14:01:28 -0700

    add all classes

--- oO_Class.js
+++ oO_Class.js
@@ -4,22 +4,19 @@ class Vehicle{
         this.model = model;
         this.year = year;
     }
-
     honk(){
         return "Beep";
     }
-
     toString(){
         return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`
     }
 }
 
 let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
-//console.log(myFirstVehicle.honk()); // "Beep."
+console.log(myFirstVehicle.honk()); // "Beep."
 
 let myFirstVeh = new Vehicle("Honda", "Monster Truck", 1999);
-//console.log(myFirstVeh.toString()); // "The vehicle is a Honda Monster Truck from 1999."
-
+console.log(myFirstVeh.toString()); // "The vehicle is a Honda Monster Truck from 1999."
 
 //              Part Two
 /* Create a class for a car. The Car class should inherit from Vehicle and each car instance should have a property 
@@ -38,13 +35,12 @@ class Car extends Vehicle{
     }   
 }
 let myFirstCar = new Car("Toyota", "Corolla", 2005);
-// console.log(myFirstCar.toString()); // "The vehicle is a Toyota Corolla from 2005."
-// console.log(myFirstCar.honk());     // "Beep."
+console.log(myFirstCar.toString()); // "The vehicle is a Toyota Corolla from 2005."
+console.log(myFirstCar.honk());     // "Beep."
+console.log(myFirstCar.numWheels);  // 4
 
-// console.log(myFirstCar.numWheels);  // 4
 
-
-//              art Three
+//              Part Three
 /*
 Create a class for a Motorcycle. This class should inherit from Vehicle and each motorcycle instance should have a 
 property called numWheels which has a value of 2. It should also have a revEngine method which returns “VROOM!!!”
@@ -72,3 +68,58 @@ console.log(myFirstMotorcycle.toString());
 console.log(myFirstMotorcycle.honk());
 console.log(myFirstMotorcycle.revEngine());
 console.log(myFirstMotorcycle.numWheels);
+
+//          Part Four
+/*
+Create a class for a Garage. It should have a property called vehicles which will store an array of vehicles, and a 
+property called capacity which is a number indicating how many vehicles will fit in the garage. When you create a 
+garage, vehicles will always be empty; you only need to provide the capacity.
+A garage should also have an add method, which attempts to add a vehicle to the array of vehicles. However, if you try
+to add something which is not a vehicle, the garage should return the message “Only vehicles are allowed in here!”. 
+Also, if the garage is at capacity, it should say “Sorry, we’re full.”
+
+let garage = new Garage(2);
+garage.vehicles; // []
+
+garage.add(new Car("Hyundai", "Elantra", 2015)); // "Vehicle added!"
+garage.vehicles; // [Car]
+
+garage.add("Taco"); // "Only vehicles are allowed in here!"
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2000));
+/ "Vehicle added!"
+garage.vehicles; // [Car, Motorcycle]
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2001));
+/ "Sorry, we're full."
+*/
+class Garage{
+    constructor(capacity){
+        this.vehicles = [];
+        this.capactiy = capacity;
+    }
+    add(vehicle){
+        if (!(vehicle instanceof Vehicle)) {
+            return "Only vehicles are allowed in here!";
+        }
+        if (this.vehicles.length >= this.capacity) {
+            //console.log("Sorry, we're full.");
+            return "Sorry, we're full.";
+        }
+        this.vehicles.push(vehicle);
+        //console.log("Vehicle added successfully");
+        return "Vehicle added successfully";     
+    }
+}
+let garage = new Garage(2);
+console.log(garage.vehicles); 
+garage.add(new Car("Hyundai", "Elantra", 2015)); 
+console.log(garage.vehicles); 
+console.log(garage.add("Taco"));  
+console.log(garage.add(new Motorcycle("Honda", "Nighthawk", 2000)));
+console.log(garage.vehicles);
+
+
+
+
+
