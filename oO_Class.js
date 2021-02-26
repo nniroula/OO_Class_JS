4e3e6c6d6f9a91e45e014c9de7cfcb594e3aa362
Nabin <nkbhaie2017@gmail.com>
2021-02-26 13:06:01 -0700

    add motorcycle class

--- oO_Class.js
+++ oO_Class.js
@@ -38,7 +38,37 @@ class Car extends Vehicle{
     }   
 }
 let myFirstCar = new Car("Toyota", "Corolla", 2005);
-console.log(myFirstCar.toString()); // "The vehicle is a Toyota Corolla from 2005."
-console.log(myFirstCar.honk());     // "Beep."
+// console.log(myFirstCar.toString()); // "The vehicle is a Toyota Corolla from 2005."
+// console.log(myFirstCar.honk());     // "Beep."
 
-console.log(myFirstCar.numWheels);  // 4)
+// console.log(myFirstCar.numWheels);  // 4
+
+
+//              art Three
+/*
+Create a class for a Motorcycle. This class should inherit from Vehicle and each motorcycle instance should have a 
+property called numWheels which has a value of 2. It should also have a revEngine method which returns “VROOM!!!”
+
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+myFirstMotorcycle.toString();
+/ "The vehicle is a Honda Nighthawk from 2000."
+
+myFirstMotorcycle.honk();     // "Beep."
+myFirstMotorcycle.revEngine(); // "VROOM!!!"
+myFirstMotorcycle.numWheels;  // 2
+*/
+
+class Motorcycle extends Vehicle{
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine(){
+        return "VROOM";
+    }
+}
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+console.log(myFirstMotorcycle.toString());
+console.log(myFirstMotorcycle.honk());
+console.log(myFirstMotorcycle.revEngine());
+console.log(myFirstMotorcycle.numWheels);
